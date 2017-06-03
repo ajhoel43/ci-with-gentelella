4752000565afa4727e330f6e8615a6d0625b3a86
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2017-06-03 17:16:13 +0700

    Update Simple Template

--- application/views/template/simple.php
+++ application/views/template/simple.php
@@ -3,8 +3,19 @@
 	<head>
 		<title><?php echo $title; ?></title>
 	</head>
+	<style>
+		#main_header {
+			text-align: center;
+			font-size: 40px;
+		}
+	</style>
 	<body>
+		<div id="main_header">
+			<?php echo $header; ?>
+		</div>
 		<?php echo $content; ?>
-		<?php echo $footer; ?>
+		<div id="main_footer" style="text-align: center;">
+			<?php echo $footer; ?>
+		</div>
 	</body>
 </html>
