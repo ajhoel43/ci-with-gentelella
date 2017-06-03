d9e03f578e9f2528a87d31b023e13edd871a72ae
Ajhoel Tragedy <pandhoe_43@yahoo.com>
2017-06-04 00:42:05 +0700

    Update Gentelella Template  - add CSS  - add JS's

--- application/views/template/gentelella.php
+++ application/views/template/gentelella.php
@@ -18,6 +18,7 @@
 		<link href="<?php echo base_url('vendors/select2/dist/css/select2.min.css') ?>" rel="stylesheet">
 		<link href="<?php echo base_url('vendors/switchery/dist/switchery.min.css') ?>" rel="stylesheet">
 		<link href="<?php echo base_url('vendors/starrr/dist/starrr.css') ?>" rel="stylesheet">
+		<link href="<?php echo base_url('vendors/jqvmap/dist/jqvmap.min.css') ?>" rel="stylesheet">
 		<link href="<?php echo base_url('vendors/bootstrap-daterangepicker/daterangepicker.css') ?>" rel="stylesheet">
 			<!-- Datatables -->
 		<link href="<?php echo base_url('vendors/datatables.net-bs/css/dataTables.bootstrap.min.css') ?>" rel="stylesheet">
@@ -50,7 +51,7 @@
 								</div>
 							</div>
 						</div>
-						<div class="clarfix"></div>
+						<div class="clearfix"></div>
 						<?php echo $content ?>
 					</div>
 				</div>
@@ -71,10 +72,31 @@
 		<script src="<?php echo base_url('vendors/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/fastclick/lib/fastclick.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/nprogress/nprogress.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/Chart.js/dist/Chart.min.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/gauge.js/dist/gauge.min.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/bootstrap-progressbar/bootstrap-progressbar.min.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/iCheck/icheck.min.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/skycons/skycons.js') ?>"></script>
+			<!-- Flot -->
+		<script src="<?php echo base_url('vendors/Flot/jquery.flot.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/Flot/jquery.flot.pie.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/Flot/jquery.flot.time.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/Flot/jquery.flot.stack.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/Flot/jquery.flot.resize.js') ?>"></script>
+			<!-- Flot Plugins -->
+		<script src="<?php echo base_url('vendors/flot.orderbars/js/jquery.flot.orderBars.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/flot-spline/js/jquery.flot.spline.min.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/flot.curvedlines/curvedLines.js') ?>"></script>
+			<!-- JQVMap -->
+		<script src="<?php echo base_url('vendors/jqvmap/dist/jquery.vmap.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/jqvmap/dist/maps/jquery.vmap.world.js') ?>"></script>
+		<script src="<?php echo base_url('vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') ?>"></script>
+
+		<script src="<?php echo base_url('vendors/DateJS/build/date.js') ?>"></script>
+			<!-- Bootstrap Date Range Picker -->
 		<script src="<?php echo base_url('vendors/moment/min/moment.min.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/bootstrap-daterangepicker/daterangepicker.js') ?>"></script>
+
 		<script src="<?php echo base_url('vendors/bootstrap-wysiwyg/js/bootstrap-wysiwyg.min.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/jquery.hotkeys/jquery.hotkeys.js') ?>"></script>
 		<script src="<?php echo base_url('vendors/google-code-prettify/src/prettify.js') ?>"></script>
